02b6bad2d16eab9caf203d4bf5694c644f03419d
Ebuka Aduebe <Ebuka.aduebe@gmail.com>
2025-03-26 22:02:20 +0000

    added configuration code for other vpc components

--- main.tf
+++ main.tf
@@ -4,41 +4,102 @@ provider "aws" {
     secret_key = ""
 }
 
-variable "dev-vpc_cidr_block" {
+variable "vpc_cidr_block" {
   description = "vpc cidr block range"
-  type = string
 }
 
-variable "dev-subnet-1_cidr_block" {
+variable "subnet-1_cidr_block" {
   description = "subnet-1 cidr block range"
-  type = string
 }
 
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = var.dev-vpc_cidr_block 
+variable "avail_zone" {
+  description = "subnet-1 availability zone"
+}
+variable env_prefix {}
+variable my_ip{}
+
+resource "aws_vpc" "nodejs-app-vpc" {
+  cidr_block = var.vpc_cidr_block 
  tags = {
-    Name: "development-vpc"
+    Name: "${var.env_prefix}-vpc"
  }
 } 
 
 
-resource "aws_subnet" "dev-subnet-1"{
-    vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.dev-subnet-1_cidr_block
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "nodejs-app-subnet"{
+    vpc_id = aws_vpc.nodejs-app-vpc.id
+    cidr_block = var.subnet-1_cidr_block
+    availability_zone = var.avail_zone
      tags = {
-    Name: "dev-subnet1"
+    Name: "${var.env_prefix}-subnet1"
  }
 }
 
-data "aws_vpc" "existing_dev_vpc" {
+data "aws_vpc" "existing-dev-vpc" {
    default = true 
 }
 
-output "dev-vpc-id" {
-    value = aws_vpc.dev-vpc.id
+output "nodejs-app-vpc-id" {
+    value = aws_vpc.nodejs-app-vpc.id
 }
 
-output "dev-subnet-id" {
-    value = aws_subnet.dev-subnet-1.id
+output "nodejs-app-subnet-id" {
+    value = aws_subnet.nodejs-app-subnet.id
+}
+
+resource "aws_route_table" "nodejs-app-route-table" {
+  vpc_id = aws_vpc.nodejs-app-vpc.id
+   route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nodejs-app-igw.id
+   }
+
+   tags = {
+    Name: "${var.env_prefix}-rtb"
+   }
+
+}
+
+resource "aws_internet_gateway" "nodejs-app-igw" {
+  vpc_id = aws_vpc.nodejs-app-vpc.id
+
+    tags = {
+    Name: "${var.env_prefix}-igw"
+   }
+}
+
+resource "aws_route_table_association" "nodejs-app-subnet-rtb-a"{
+  subnet_id = aws_subnet.nodejs-app-subnet.id
+  route_table_id = aws_route_table.nodejs-app-route-table.id
+}
+
+resource "aws_security_group" "nodejs-app-sg" {
+  name =  "nodejs-app-sg"
+  vpc_id = aws_vpc.nodejs-app-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+   ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+    tags = {
+    Name: "${var.env_prefix}-sg"
+   }
 }
